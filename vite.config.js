ef2595fd6f1ef157649fc56b63f94c98c75a7b81
Monicah Koina <monicahkoina1@gmail.com>
2025-09-02 23:31:16 +0300

    chakra ui

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import jsconfigPaths from 'vite-jsconfig-paths'
+// import jsconfigPaths from 'vite-jsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
-  jsconfigPaths()
+    // jsconfigPaths()
   ],
 })
