119c74ddf5bfd955a5d0fc62c04b0a0f7cc7be1c
Monicah Koina <monicahkoina1@gmail.com>
2025-09-02 23:31:57 +0300

    routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import Hero from "../pages/Hero";
+import CashierDashboard from "../pages/cashier/CashierDashboard";
+import RecordTransaction from "../pages/cashier/RecordTransaction";
+import ProcurementDashboard from "../pages/procurementOfficer/ProcurementDashboard";
+import Notifications from "../pages/procurementOfficer/needIdentification/Notifications";
 
 function AppRoutes() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route path="cashier-dashboard" element={<CashierDashboard />}>
+          <Route path="record-transaction" element={<RecordTransaction />} />
+        </Route>
+        <Route path="procurement-dashboard" element={<ProcurementDashboard />}>
+          <Route path="notifications" element={<Notifications />}></Route>
+        </Route>
       </Routes>
     </div>
   );
