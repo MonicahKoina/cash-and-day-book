ee55e14e35e822ea57ae71f01d8d8a6daa30d456
Monicah Koina <monicahkoina1@gmail.com>
2025-09-02 23:31:38 +0300

    disable chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
-import { Provider } from "./components/ui/provider";
 
 createRoot(document.getElementById("root")).render(
-  <Provider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
