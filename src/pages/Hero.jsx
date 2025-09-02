f3ce08eb8f218e2151a51ed87332138cbdab17b8
Monicah Koina <monicahkoina1@gmail.com>
2025-09-02 23:32:42 +0300

    hero page

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,28 +1,36 @@
-import { Button } from "@chakra-ui/react";
 import React from "react";
 import { useNavigate } from "react-router";
 function Hero() {
   const navigate = useNavigate();
   return (
-    <div className="flex w-full justify-center flex-col items-center h-[100vh] ">
-      <div className="text-center ">
+    <div className="flex w-full justify-center flex-col items-center h-[100vh] bg-gradient-to-b from-blue-100 to-blue-50  ">
+      <div className="text-center text-4xl font-bold ">
         <h1>Welcome to Cash an Day books</h1>
         <h2>Manage you daily cash & bank records with ease</h2>
         <p>Select your role to continue</p>
       </div>
       <div
-        className=" flex flex-col gap-2 md:flex justify-between "
+        className=" flex flex-col gap-2 md:flex justify-between text-2xl "
         style={{ margin: "20px" }}
       >
-        <Button colorPalette={"blue"} variant="outline">
+        <button
+          className="border border-gray-400 rounded shadow-2xl bg-gradient-to-bl from-blue-200 to-blue-50 w-80 "
+          onClick={() => navigate("/cashier-dashboard")}
+        >
           Cashier
-        </Button>
-        <Button colorPalette={"green"} variant="outline">
-          Procurement Officer
-        </Button>
-        <Button colorPalette={"teal"} variant="outline">
+        </button>
+        <button
+          onClick={() => navigate("/procurement-dashboard")}
+          className="border border-gray-400 rounded shadow-2xl bg-gradient-to-bl from-green-200 to-green-50  "
+        >
+          Procurement officer
+        </button>
+        <button
+          onClick={() => navigate("/accountant-dashboard")}
+          className="border border-gray-400 rounded shadow-2xl bg-gradient-to-bl from-amber-200 to-amber-50 "
+        >
           Accountant
-        </Button>
+        </button>
       </div>
     </div>
   );
